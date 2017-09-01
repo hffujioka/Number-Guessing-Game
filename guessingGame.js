9e9d92fa2ed4893739c73283b67a8f2ee2e22771
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-09-01 08:54:31 -1000

    Update guessingGame.js

--- guessingGame.js
+++ guessingGame.js
@@ -12,3 +12,17 @@ else {
     window.alert("You chose " + playerTwo);
   }
 }
+//two player number guessing game//
+
+
+
+var playerOne = (Math.floor(Math.random() * 6) + 1);
+var playerTwo = prompt("Player 2: Guess a number between 1 and 6.");
+  if (playerOne == playerTwo) {
+  	window.alert("You win!");
+  }
+  else {
+  	window.alert("The answer was " + playerOne);
+    window.alert("You chose " + playerTwo);
+  }
+//computer generated number guessing game
