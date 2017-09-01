94055e1ae09d128d0d1c146da800a78cf777cd92
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-09-01 09:21:11 -1000

    Create forLoops.js

---
+++ forLoops.js
@@ -0,0 +1,11 @@
+var foods = [
+	"sushi",
+  "ramen",
+  "gyoza",
+  "curry"
+]
+
+for (var i = 0; i<foods.length; i++){
+	window.alert(foods[i])
+}
+//call items in an array to the screen one by one//
