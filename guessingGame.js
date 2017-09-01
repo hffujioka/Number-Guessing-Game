74006479259281e38b677115eacfeb538c5f0e2a
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-09-01 08:32:48 -1000

    Create guessingGame.js

---
+++ guessingGame.js
@@ -0,0 +1,14 @@
+var playerOne = prompt("Player 1: Choose a number between 1 and 6.");
+if ((playerOne < 1) || (playerOne > 6)) {
+	window.alert("Invalid entry");
+}
+else {
+	var playerTwo = prompt("Player 2: Guess a number between 1 and 6.");
+  if (playerOne == playerTwo) {
+  	window.alert("You win!");
+  }
+  else {
+  	window.alert("Player one answer was " + playerOne);
+    window.alert("You chose " + playerTwo);
+  }
+}
